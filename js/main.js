afe50af15e928a98249fb822d2541dc0aae9a548
robertoraciti <rob.raciti97@gmail.com>
2023-09-06 18:01:38 +0200

    Complete ex without bonus

--- js/main.js
+++ js/main.js
@@ -3,15 +3,25 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      email: "Hello Vue!",
+      emails: [],
     };
   },
 
+  methods: {
+    generateRandomEmail() {
+      for (let i = 0; i < 10; i++) {
+        axios
+          .get("https://flynn.boolean.careers/exercises/api/random/mail")
+          .then((result) => {
+            console.log(result.data.response);
+            const email = result.data.response;
+            this.emails.push(email);
+          });
+      }
+    },
+  },
+
   mounted() {
-    axios
-      .get("https://flynn.boolean.careers/exercises/api/random/mail")
-      .then((result) => {
-        console.log(result.data.response);
-      });
+    this.generateRandomEmail();
   },
 }).mount("#app");
